b7742922bbe65b6a82cca10b4e02e13926673de6
kejiahp <popoolakejiah@gmail.com>
2025-10-29 18:20:02 +0000

    fix: windows build fix

--- winbuild.bat
+++ winbuild.bat
@@ -4,7 +4,7 @@ cd build
 cmake .. -G "Visual Studio 17 2022" -A x64
 @if ERRORLEVEL 1 goto onError
 
-cmake --build • --config Release
+cmake --build . --config Release
 @if ERRORLEVEL 1 goto onError
 
 goto onSuccess
